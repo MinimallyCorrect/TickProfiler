c9ef38028fdca34852d253fb0bba00d619481b71
Ross Allan <rallanpcl@gmail.com>
2013-07-08 14:51:02 +0100

    Fix #5
    Signed-off-by: Ross Allan <rallanpcl@gmail.com>

--- src/common/me/nallar/tickprofiler/util/contextaccess/ContextAccessProvider.java
+++ src/common/me/nallar/tickprofiler/util/contextaccess/ContextAccessProvider.java
@@ -14,11 +14,21 @@ class ContextAccessProvider {
 				ContextAccess contextAccess = (ContextAccess) clazz.newInstance();
 				Class<?> currentClass = contextAccess.getContext(0);
 				if (currentClass != ContextAccessProvider.class) {
-					throw new Error("Wrong class returned: " + currentClass + ", expected ContextAccessProvider");
+					StringBuilder sb = new StringBuilder();
+					sb.append("Stack:\n");
+					for (int i = -2; i < 3; i++) {
+						try {
+							sb.append(contextAccess.getContext(i).getName()).append(" at ").append(i).append('\n');
+						} catch (Throwable ignored) {
+						}
+					}
+					throw new Error("Wrong class returned: " + currentClass + ", expected ContextAccessProvider. " + sb);
 				}
 				return contextAccess;
 			} catch (Throwable t) {
-				Log.warning("Failed to instantiate " + clazz, t);
+				if (Log.debug) {
+					Log.debug("Unable to set up context access class " + clazz + ". " + t.getMessage() + ", falling back to slower context access. On JRE: " + System.getProperty("java.version"));
+				}
 			}
 		}
 		throw new Error("Failed to set up any context access");
