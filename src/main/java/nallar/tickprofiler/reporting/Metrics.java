74f736d43b8f5d0dd859e3cca44fc2d4100a28c6
Ross Allan <ross@nallar.me>
2016-02-12 16:46:53 +0000

    Use 15 minute ping interval for metrics (now the default)

--- src/main/java/nallar/tickprofiler/reporting/Metrics.java
+++ src/main/java/nallar/tickprofiler/reporting/Metrics.java
@@ -78,7 +78,7 @@ public class Metrics {
 	/**
 	 * Interval of time to ping (in minutes)
 	 */
-	private static final int PING_INTERVAL = 10;
+	private static final int PING_INTERVAL = 15;
 	/**
 	 * The mod this metrics submits for
 	 */
