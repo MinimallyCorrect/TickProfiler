a53cb5c891722316a833be5ade0852d58d8ef017
Ross Allan <ross@nallar.me>
2016-06-25 16:56:29 +0100

    Update required ModPatcher version to 1.8.9.92

--- src/main/java/nallar/tickprofiler/minecraft/CoreMod.java
+++ src/main/java/nallar/tickprofiler/minecraft/CoreMod.java
@@ -11,7 +11,7 @@ import java.util.*;
 @IFMLLoadingPlugin.SortingIndex(1001)
 public class CoreMod implements IFMLLoadingPlugin {
 	static {
-		ModPatcher.requireVersion("1.8.9.81");
+		ModPatcher.requireVersion("1.8.9.92");
 	}
 
 	@Override
