47ba5e73b9cb0c716c3d7dec485ed632c7af59c4
Ross Allan <rossallan3@gmail.com>
2018-08-12 18:45:47 +0100

    Potential fix for incorrect CPU usage percentage (see #102)

--- src/main/java/org/minimallycorrect/tickprofiler/minecraft/profiling/UtilisationProfiler.java
+++ src/main/java/org/minimallycorrect/tickprofiler/minecraft/profiling/UtilisationProfiler.java
@@ -8,11 +8,8 @@ import java.util.stream.*;
 import lombok.val;
 
 import org.minimallycorrect.tickprofiler.util.CollectionsUtil;
-import org.minimallycorrect.tickprofiler.util.TableFormatter;
 
 public class UtilisationProfiler extends Profile {
-	private final Map<String, Long> monitorMap = new HashMap<>();
-
 	@Override
 	public void start() {
 		val threadMxBean = ManagementFactory.getThreadMXBean();
@@ -20,42 +17,54 @@ public class UtilisationProfiler extends Profile {
 		if (elements <= 0)
 			throw new IllegalArgumentException("elements must be > 0");
 
-		start(() -> threadMxBean.setThreadCpuTimeEnabled(true), () -> {
+		val initialTimes = new HashMap<String, Long>();
+
+		start(() -> {
+			threadMxBean.setThreadCpuTimeEnabled(true);
+
+			List<Long> threads = Thread.getAllStackTraces().keySet().stream().map(Thread::getId).collect(Collectors.toList());
+			getThreadCpuTimes(threadMxBean, threads, initialTimes);
+		}, () -> {
 			List<Long> threads = Thread.getAllStackTraces().keySet().stream().map(Thread::getId).collect(Collectors.toList());
 
-			for (Long threadId_ : threads) {
-				long threadId = threadId_;
-				long time = threadMxBean.getThreadCpuTime(threadId);
-				if (time < 0) {
-					continue;
-				}
-				ThreadInfo thread = threadMxBean.getThreadInfo(threadId, 0);
-				if (thread != null) {
-					monitorMap.put(thread.getThreadName(), time);
-				}
+			val endTimes = new HashMap<String, Long>();
+			getThreadCpuTimes(threadMxBean, threads, endTimes);
+
+			val endDurations = new HashMap<String, Long>();
+			for (val e : endTimes.entrySet()) {
+				endDurations.put(e.getKey(), e.getValue() - initialTimes.getOrDefault(e.getKey(), 0L));
 			}
 
 			targets.forEach(it -> {
 				val tf = it.getTableFormatter();
-				dump(tf, elements);
+				double runTimeNanoSeconds = TimeUnit.SECONDS.toNanos(runTimeSeconds);
+				tf
+					.heading("Thread")
+					.heading("Used CPU Time (%)");
+				for (String key : CollectionsUtil.sortedKeys(endDurations, elements)) {
+					tf
+						.row(key)
+						.row((100d * endDurations.get(key)) / runTimeNanoSeconds);
+				}
+				tf.finishTable();
 				it.sendTables(tf);
 			});
 		}, () -> {
 			threadMxBean.setThreadCpuTimeEnabled(false);
-			monitorMap.clear();
 		});
 	}
 
-	private void dump(final TableFormatter tf, int entries) {
-		double seconds = TimeUnit.SECONDS.toNanos(runTimeSeconds);
-		tf
-			.heading("Thread")
-			.heading("Used CPU Time (%)");
-		for (String key : CollectionsUtil.sortedKeys(monitorMap, entries)) {
-			tf
-				.row(key)
-				.row((100d * monitorMap.get(key)) / seconds);
+	private void getThreadCpuTimes(ThreadMXBean threadMxBean, List<Long> threads, HashMap<String, Long> map) {
+		for (Long threadId_ : threads) {
+			long threadId = threadId_;
+			long time = threadMxBean.getThreadCpuTime(threadId);
+			if (time < 0) {
+				continue;
+			}
+			ThreadInfo thread = threadMxBean.getThreadInfo(threadId, 0);
+			if (thread != null) {
+				map.put(thread.getThreadName(), time);
+			}
 		}
-		tf.finishTable();
 	}
 }
